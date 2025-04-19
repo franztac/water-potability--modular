5f3f4723a3d738b28daeb3b98a7a2dcb89f45049
franztac <fratacc91@gmail.com>
2025-04-19 17:09:36 +0200

    try-except blocks added

--- src/data_collection.py
+++ src/data_collection.py
@@ -10,35 +10,47 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 def load_data(filepath: str) -> pd.DataFrame:
-    logging.info(f"----- {filepath} loaded")
-    return pd.read_csv(filepath)
+    try:
+        logging.info(f"----- {filepath} loaded")
+        return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f"*** Error loading data from {filepath}: {e}")
 
 
 # df = pd.read_csv(r"C:/Users/UTENTE/Git Repos/water_potability.csv")
 
 
 def load_params(filepath: str) -> float:
-    with open(filepath, "r") as file:
-        params = yaml.safe_load(file)
-        logging.info(f"----- test_size loaded from {filepath}")
-        return params["data_collection"]["test_size"]
+    try:
+        with open(filepath, "r") as file:
+            params = yaml.safe_load(file)
+            logging.info(f"----- params: test_size loaded from {filepath}")
+            return params["data_collection"]["test_size"]
+    except Exception as e:
+        raise Exception(f"*** Error loading parameters from {filepath}: {e}")
 
 
 # test_size = params["data_collection"]["test_size"]
 
 
 def split_data(df: pd.DataFrame, test_size: float) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    logging.info(f"----- train test split with test size of {test_size * 100} %")
-    train_df, test_df = train_test_split(df, test_size=test_size, random_state=42)
-    return train_df, test_df
+    try:
+        logging.info(f"----- splitting data with test size of {test_size * 100} %")
+        train_df, test_df = train_test_split(df, test_size=test_size, random_state=42)
+        return train_df, test_df
+    except Exception as e:
+        raise Exception(f"*** Error splitting dataset: {e}")
 
 
 # train_df, test_df = train_test_split(df, test_size=test_size, random_state=42)
 
 
 def save_data(df: pd.DataFrame, filepath: str) -> None:
-    logging.info(f"----- saving data to {filepath}")
-    df.to_csv(filepath, index=False)
+    try:
+        logging.info(f"----- saving data to {filepath}")
+        df.to_csv(filepath, index=False)
+    except Exception as e:
+        raise Exception(f"*** Error saving data to {filepath}: {e}")
 
 
 # train_df.to_csv(os.path.join(data_path, "train.csv"), index=False)
@@ -46,19 +58,23 @@ def save_data(df: pd.DataFrame, filepath: str) -> None:
 
 
 def main():
-    params_filepath = "params.yaml"
-    data_filepath = "C:/Users/UTENTE/Git Repos/water_potability.csv"
-    raw_datapath = os.path.join("data", "raw")
+    try:
+        params_filepath = "params.yaml"
+        data_filepath = "C:/Users/UTENTE/Git Repos/water_potability.csv"
+        raw_datapath = os.path.join("data", "raw")
 
-    test_size = load_params(params_filepath)
-    df = load_data(data_filepath)
-    train_df, test_df = split_data(df, test_size=test_size)
+        test_size = load_params(params_filepath)
+        df = load_data(data_filepath)
+        train_df, test_df = split_data(df, test_size=test_size)
 
-    logging.info(f"----- creating folder {raw_datapath}")
-    os.makedirs(raw_datapath, exist_ok=True)
+        logging.info(f"----- creating folder {raw_datapath}")
+        os.makedirs(raw_datapath, exist_ok=True)
 
-    save_data(train_df, os.path.join(raw_datapath, "train.csv"))
-    save_data(test_df, os.path.join(raw_datapath, "test.csv"))
+        save_data(train_df, os.path.join(raw_datapath, "train.csv"))
+        save_data(test_df, os.path.join(raw_datapath, "test.csv"))
+
+    except Exception as e:
+        raise Exception(f"**** An error occurred: {e}")
 
 
 if __name__ == "__main__":

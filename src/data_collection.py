8af74d25af2201286ed66392e4991a0c637b80de
franztac <fratacc91@gmail.com>
2025-04-19 18:10:26 +0200

    data pre-processing done

--- src/data_collection.py
+++ src/data_collection.py
@@ -6,15 +6,18 @@ from typing import Tuple
 import logging
 
 
-logging.basicConfig(level=logging.INFO, format="%(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="[%(levelname)s - %(filename)s] %(message)s"
+)
 
 
 def load_data(filepath: str) -> pd.DataFrame:
     try:
-        logging.info(f"----- {filepath} loaded")
+        logging.info(f"{filepath} loaded")
         return pd.read_csv(filepath)
     except Exception as e:
-        raise Exception(f"*** Error loading data from {filepath}: {e}")
+        logging.error(f"Error loading data from {filepath}: {e}")
+        raise
 
 
 # df = pd.read_csv(r"C:/Users/UTENTE/Git Repos/water_potability.csv")
@@ -24,10 +27,11 @@ def load_params(filepath: str) -> float:
     try:
         with open(filepath, "r") as file:
             params = yaml.safe_load(file)
-            logging.info(f"----- params: test_size loaded from {filepath}")
+            logging.info(f"params: test_size loaded from {filepath}")
             return params["data_collection"]["test_size"]
     except Exception as e:
-        raise Exception(f"*** Error loading parameters from {filepath}: {e}")
+        logging.error(f"Error loading parameters from {filepath}: {e}")
+        raise
 
 
 # test_size = params["data_collection"]["test_size"]
@@ -35,11 +39,12 @@ def load_params(filepath: str) -> float:
 
 def split_data(df: pd.DataFrame, test_size: float) -> Tuple[pd.DataFrame, pd.DataFrame]:
     try:
-        logging.info(f"----- splitting data with test size of {test_size * 100} %")
+        logging.info(f"splitting data with test size of {test_size * 100} %")
         train_df, test_df = train_test_split(df, test_size=test_size, random_state=42)
         return train_df, test_df
     except Exception as e:
-        raise Exception(f"*** Error splitting dataset: {e}")
+        logging.error(f"Error splitting dataset: {e}")
+        raise
 
 
 # train_df, test_df = train_test_split(df, test_size=test_size, random_state=42)
@@ -47,10 +52,11 @@ def split_data(df: pd.DataFrame, test_size: float) -> Tuple[pd.DataFrame, pd.Dat
 
 def save_data(df: pd.DataFrame, filepath: str) -> None:
     try:
-        logging.info(f"----- saving data to {filepath}")
+        logging.info(f"saving data to {filepath}")
         df.to_csv(filepath, index=False)
     except Exception as e:
-        raise Exception(f"*** Error saving data to {filepath}: {e}")
+        logging.error(f"Error saving data to {filepath}: {e}")
+        raise
 
 
 # train_df.to_csv(os.path.join(data_path, "train.csv"), index=False)
@@ -67,14 +73,15 @@ def main():
         df = load_data(data_filepath)
         train_df, test_df = split_data(df, test_size=test_size)
 
-        logging.info(f"----- creating folder {raw_datapath}")
+        logging.info(f"creating folder {raw_datapath}")
         os.makedirs(raw_datapath, exist_ok=True)
 
         save_data(train_df, os.path.join(raw_datapath, "train.csv"))
         save_data(test_df, os.path.join(raw_datapath, "test.csv"))
 
     except Exception as e:
-        raise Exception(f"**** An error occurred: {e}")
+        logging.error(f"An error occurred: {e}")
+        raise
 
 
 if __name__ == "__main__":
--- src/data_prep.py
+++ src/data_prep.py
@@ -1,28 +1,72 @@
 import pandas as pd
 import numpy as np
 import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="[%(levelname)s - %(filename)s] %(message)s"
+)
 
 
-train_df = pd.read_csv("./data/raw/train.csv")
-test_df = pd.read_csv("./data/raw/test.csv")
+def load_data(filepath):
+    try:
+        logging.info(f"loading: {filepath}")
+        return pd.read_csv(filepath)
+    except Exception as e:
+        logging.error(f"error loading data from {filepath}: {e}")
+        raise
+
+
+# train_df = pd.read_csv("./data/raw/train.csv")
+# test_df = pd.read_csv("./data/raw/test.csv")
 
 
 def fill_missing_values_with_median(df_original):
-    df = df_original.copy()
-    for column in df.columns:
-        if df[column].isna().any():
-            median_value = df[column].median()
-            df[column] = df[column].fillna(median_value)
+    try:
+        logging.info("filling missing values with median...")
+        df = df_original.copy()
+        for column in df.columns:
+            if df[column].isna().any():
+                median_value = df[column].median()
+                df[column] = df[column].fillna(median_value)
 
-    return df
+        return df
+
+    except Exception as e:
+        logging.error(f"error filling missing values: {e}")
+        raise
 
 
-# Fill missing values with median
-train_processed_df = fill_missing_values_with_median(train_df)
-test_processed_df = fill_missing_values_with_median(test_df)
+def save_data(df, filepath):
+    try:
+        logging.info(f"saving data to {filepath}")
+        return df.to_csv(filepath, index=False)
+    except Exception as e:
+        logging.error(f"error saving data to {filepath}: {e}")
 
-data_path = os.path.join("data", "processed")
-os.makedirs(data_path, exist_ok=True)
 
-train_processed_df.to_csv(os.path.join(data_path, "train_processed.csv"), index=False)
-test_processed_df.to_csv(os.path.join(data_path, "test_processed.csv"), index=False)
+# train_processed_df.to_csv(os.path.join(data_path, "train_processed.csv"), index=False)
+# test_processed_df.to_csv(os.path.join(data_path, "test_processed.csv"), index=False)
+
+
+def main():
+    raw_datapath = "./data/raw"
+    processed_datapath = "./data/processed"
+
+    train_df = load_data(os.path.join(raw_datapath, "train.csv"))
+    test_df = load_data(os.path.join(raw_datapath, "test.csv"))
+
+    train_processed_df = fill_missing_values_with_median(train_df)
+    test_processed_df = fill_missing_values_with_median(test_df)
+
+    logging.info(f"creating {processed_datapath} folder")
+    os.makedirs(processed_datapath, exist_ok=True)
+
+    save_data(
+        train_processed_df, os.path.join(processed_datapath, "train_processed.csv")
+    )
+    save_data(test_processed_df, os.path.join(processed_datapath, "test_processed.csv"))
+
+
+if __name__ == "__main__":
+    main()
